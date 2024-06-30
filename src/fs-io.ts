7c4c209b39d7390bc36653f7646192b07d2b571e
Kareem <141845998+RealKareemAnees@users.noreply.github.com>
2024-06-30 10:06:21 +0300

    feat: created writefile method

--- src/fs-io.ts
+++ src/fs-io.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as fs from "fs";
+import { join } from "path";
+import type { Readable } from "stream";
 
 @Injectable()
-export class FsIo {}
+export class FsIo {
+    constructor(private configService: ConfigService) {}
+
+    createReadStream(path: string): Readable {
+        return fs.createReadStream(path);
+    }
+    writeFile(
+        content: Readable,
+        format?: string,
+        filename?: string,
+        dist?: string,
+    ): string {
+        if (!dist) dist = this.configService.get("DEFAULT_DIR");
+        if (!filename)
+            filename = this.configService.get("DEFAULT_FILENAME") + "";
+        if (!format) format = "mp4";
+
+        const path = join(dist, filename + "." + format);
+
+        const writeStream = fs.createWriteStream(path, { flags: "w+" });
+
+        content.pipe(writeStream);
+
+        content.on("end", () => {
+            writeStream.close();
+        });
+
+        content.on("error", (err) => {
+            writeStream.close();
+            throw err;
+        });
+
+        return path;
+    }
+}
